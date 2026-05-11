f6bc8a0034699a059073fbac6d2e0fcb1c685f3a
İkranurAkpinar <ikranurakpinar0@gmail.com>
2026-05-12 00:23:09 +0300

    yeni güncelleme

---
+++ NovaKitap/Migrations/20260506122851_init.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NovaKitap.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Adresler",
+                columns: table => new
+                {
+                    AdresId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    KullaniciId = table.Column<int>(type: "int", nullable: false),
+                    AdresBasligi = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AcikAdres = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Sehir = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Adresler", x => x.AdresId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kartlar",
+                columns: table => new
+                {
+                    KartId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    KullaniciId = table.Column<int>(type: "int", nullable: false),
+                    KartBasligi = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AdSoyad = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    KartNumarasi = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SonKullanmaAy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SonKullanmaYil = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kartlar", x => x.KartId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kategoriler",
+                columns: table => new
+                {
+                    KategoriId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    KategoriAdi = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    IkonSifi = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Kategori__1782CC72FD64979E", x => x.KategoriId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kullanicilar",
+                columns: table => new
+                {
+                    KullaniciId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AdSoyad = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Sifre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Kullanic__E011F77B953AA4E8", x => x.KullaniciId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Siparisler",
+                columns: table => new
+                {
+                    SiparisId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    KullaniciId = table.Column<int>(type: "int", nullable: false),
+                    SiparisTarihi = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ToplamTutar = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    SiparisDurumu = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    KargoAdresi = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Siparisler", x => x.SiparisId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Yazarlar",
+                columns: table => new
+                {
+                    YazarId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AdSoyad = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Biyografi = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Yazarlar__CF636CE1C5425819", x => x.YazarId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kitaplar",
+                columns: table => new
+                {
+                    KitapId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    KitapAdi = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Aciklama = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SayfaSayisi = table.Column<int>(type: "int", nullable: true),
+                    Fiyat = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    KapakResimUrl = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    YeniCikanMi = table.Column<bool>(type: "bit", nullable: true, defaultValue: false),
+                    CokSatanMi = table.Column<bool>(type: "bit", nullable: true, defaultValue: false),
+                    YazarId = table.Column<int>(type: "int", nullable: true),
+                    KategoriId = table.Column<int>(type: "int", nullable: true),
+                    Yayinevi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BasimYili = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Kitaplar__89491B0C9F34EF61", x => x.KitapId);
+                    table.ForeignKey(
+                        name: "FK__Kitaplar__Katego__5165187F",
+                        column: x => x.KategoriId,
+                        principalTable: "Kategoriler",
+                        principalColumn: "KategoriId");
+                    table.ForeignKey(
+                        name: "FK__Kitaplar__YazarI__5070F446",
+                        column: x => x.YazarId,
+                        principalTable: "Yazarlar",
+                        principalColumn: "YazarId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kaydedilenler",
+                columns: table => new
+                {
+                    KayitId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    KullaniciId = table.Column<int>(type: "int", nullable: true),
+                    KitapId = table.Column<int>(type: "int", nullable: true),
+                    KayitTarihi = table.Column<DateTime>(type: "datetime", nullable: true, defaultValueSql: "(getdate())")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Kaydedil__BD28AF4BD0CD6182", x => x.KayitId);
+                    table.ForeignKey(
+                        name: "FK__Kaydedile__Kitap__5812160E",
+                        column: x => x.KitapId,
+                        principalTable: "Kitaplar",
+                        principalColumn: "KitapId");
+                    table.ForeignKey(
+                        name: "FK__Kaydedile__Kulla__571DF1D5",
+                        column: x => x.KullaniciId,
+                        principalTable: "Kullanicilar",
+                        principalColumn: "KullaniciId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SiparisDetaylari",
+                columns: table => new
+                {
+                    SiparisDetayId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SiparisId = table.Column<int>(type: "int", nullable: false),
+                    KitapId = table.Column<int>(type: "int", nullable: false),
+                    Adet = table.Column<int>(type: "int", nullable: false),
+                    BirimFiyat = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SiparisDetaylari", x => x.SiparisDetayId);
+                    table.ForeignKey(
+                        name: "FK_SiparisDetaylari_Kitaplar_KitapId",
+                        column: x => x.KitapId,
+                        principalTable: "Kitaplar",
+                        principalColumn: "KitapId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SiparisDetaylari_Siparisler_SiparisId",
+                        column: x => x.SiparisId,
+                        principalTable: "Siparisler",
+                        principalColumn: "SiparisId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Kaydedilenler_KitapId",
+                table: "Kaydedilenler",
+                column: "KitapId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Kaydedilenler_KullaniciId",
+                table: "Kaydedilenler",
+                column: "KullaniciId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Kitaplar_KategoriId",
+                table: "Kitaplar",
+                column: "KategoriId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Kitaplar_YazarId",
+                table: "Kitaplar",
+                column: "YazarId");
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__Kullanic__A9D1053422C2AF0E",
+                table: "Kullanicilar",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SiparisDetaylari_KitapId",
+                table: "SiparisDetaylari",
+                column: "KitapId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SiparisDetaylari_SiparisId",
+                table: "SiparisDetaylari",
+                column: "SiparisId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Adresler");
+
+            migrationBuilder.DropTable(
+                name: "Kartlar");
+
+            migrationBuilder.DropTable(
+                name: "Kaydedilenler");
+
+            migrationBuilder.DropTable(
+                name: "SiparisDetaylari");
+
+            migrationBuilder.DropTable(
+                name: "Kullanicilar");
+
+            migrationBuilder.DropTable(
+                name: "Kitaplar");
+
+            migrationBuilder.DropTable(
+                name: "Siparisler");
+
+            migrationBuilder.DropTable(
+                name: "Kategoriler");
+
+            migrationBuilder.DropTable(
+                name: "Yazarlar");
+        }
+    }
+}
